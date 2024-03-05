55dcabadc42aab6210006622aea556b01d28b89b
EPICGameGuy <admin@cactus.vg>
2024-03-05 11:56:40 -0500

    Added extra info for connecting clients

--- unreal-todo-list/src/auth.rs
+++ unreal-todo-list/src/auth.rs
@@ -3,18 +3,47 @@ use tokio_tungstenite::tungstenite::{
     http,
 };
 
+/*
+Request headers: {
+    "host": "analytics.cactus.vg",
+    "accept-encoding": "gzip",
+    "authorization": "f7965ead-a6ea-43a8-bf30-d2ab5b22e533",
+    "cache-control": "no-cache",
+    "cdn-loop": "cloudflare",
+    "cf-connecting-ip": "2600:1700:8df7:29f:7021:2ea0:da74:5336",
+    "cf-ipcountry": "US",
+    "cf-ray": "85fb5c6dc9816783-ATL",
+    "cf-visitor": "{\"scheme\":\"https\"}",
+    "cf-warp-tag-id": "f93f0d56-52f7-46fc-b912-0c3885f8d656",
+    "connection": "Upgrade",
+    "origin": "http://analytics.cactus.vg",
+    "pragma": "no-cache",
+    "sec-websocket-key": "NxaSCFMctTc+Nw5y+/JGzA==",
+    "sec-websocket-version": "13",
+    "upgrade": "websocket",
+    "x-forwarded-for": "2600:1700:8df7:29f:7021:2ea0:da74:5336",
+    "x-forwarded-proto": "https",
+}
+ */
+fn get_connection_info(req: &Request) -> String {
+    let mut out_string = String::new();
+
+    if let Some(ip) = req.headers().get("cf-connecting-ip") {
+        out_string += &format!("\n\tip: {}", ip.to_str().unwrap_or(""));
+    }
+
+    if let Some(country) = req.headers().get("cf-ipcountry") {
+        out_string += &format!("\n\tcountry: {}", country.to_str().unwrap_or(""));
+    }
+
+    out_string
+}
+
 // Authorizes a websocket connection or drops the connection if unauthorized
 pub fn authorize(
     req: &Request,
     mut response: Response,
 ) -> Result<http::Response<()>, http::Response<Option<String>>> {
-    // println!("Received a new ws handshake");
-    // println!("The request's path is: {}", req.uri().path());
-    // println!("The request's headers are:");
-    // for (ref header, _value) in req.headers() {
-    //     println!("* {}: {:?}", header, _value);
-    // }
-
     let mk_err = || {
         Response::builder()
             .status(tokio_tungstenite::tungstenite::http::StatusCode::UNAUTHORIZED)
@@ -26,7 +55,10 @@ pub fn authorize(
 
     let ref auth_key = crate::get_server_state().secrets.keys.todolist_auth_key;
     if auth == auth_key {
-        println!("Authorized socket connection");
+        println!(
+            "Authorized socket connection for: {}",
+            get_connection_info(req)
+        );
         Ok(response)
     } else {
         println!("Unauthorized socket connection, dropping!");
